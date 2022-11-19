7f2b3b4d600bc7d52518eb91236b9867ecef19a6
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-11-19 19:11:56 -0300

    commit

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const pessoasRoute = require('./pessoasRoute');
+const niveisRoute = require('./niveisRoute');
+const turmasRoute = require('./turmasRoute');
 
 module.exports = app => {
-    app.use(bodyParser.json(), pessoasRoute);
+    app.use(bodyParser.json(), pessoasRoute, niveisRoute, turmasRoute);
 };
