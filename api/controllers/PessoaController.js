cb396cedfb98565cc4023302b348edf3331d3e78
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-12-17 10:45:42 -0300

    commit

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -122,20 +122,6 @@ class PessoaController {
         }
     }
 
-    static async pegaMatriculas(req, res) {
-        const { estudanteId } = req.params;
-
-        try {
-            const pessoa = await database.Pessoas.findOne({ where: { id: Number(estudanteId) } });
-
-            const matriculas = await pessoa.getAulasMatriculadas();
-
-            return res.status(200).json(matriculas);
-        } catch(error) {
-            return res.status(500).json(error.message);
-        }
-    }
-
     static async restauraMatricula(req, res) {
         const { estudanteId, matriculaId } = req.params;
 
@@ -163,6 +149,38 @@ class PessoaController {
             });
 
             return res.status(200).json({ message: `id ${matriculaId} deletado` });
+        } catch(error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async pegaMatriculas(req, res) {
+        const { estudanteId } = req.params;
+
+        try {
+            const pessoa = await database.Pessoas.findOne({ where: { id: Number(estudanteId) } });
+
+            const matriculas = await pessoa.getAulasMatriculadas();
+
+            return res.status(200).json(matriculas);
+        } catch(error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async pegaMatriculasPorTurma(req, res) {
+        const { turmaId } = req.params;
+
+        try {
+            const todasAsMatriculas = await database.Matriculas.findAndCountAll({ 
+                where: { 
+                    turma_id: Number(turmaId), 
+                    status: 'confirmado' 
+                },
+                limit: 1,
+                order: [['estudante_id', 'DESC']]
+            });
+            return res.status(200).json(todasAsMatriculas);
         } catch(error) {
             return res.status(500).json(error.message);
         }
